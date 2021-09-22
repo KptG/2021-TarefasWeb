c7a81baa710287aceb15e135b7dd06976f45adfe
KptG <joaovictors2010@gmail.com>
2021-09-21 22:49:04 -0300

    pagina index componentizada

---
+++ src/components/AppCardBullets.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const AppCardBullets = props =>(
+
+    <div>
+   
+      <hr />
+      <h5>{props.date}</h5>
+   
+      <ul>
+        {props.bullets.map(bullet => (<li><strong>{bullet.type}</strong>{bullet.title}</li>))}
+      </ul>
+      <hr />
+   
+    </div>
+    );
+
+    export default AppCardBullets;
